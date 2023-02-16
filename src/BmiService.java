07a1380ed98852bcf6d5d645f5f661e44b70e7a3
Komarov <komaroff74@yandex.ru>
2023-02-17 01:03:19 +0500

    initial commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,8 @@
 public class BmiService {
-    public double calculate (double weight, double height){
+    public double calculate(double weight, double height) {
         double index = (weight / (height * height));
-        return  index;
+        return index;
     }
 
-    }
+}
 
